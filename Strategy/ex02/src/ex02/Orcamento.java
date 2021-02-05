76c7da3b3f6663a957a9783e68f68ffee8d2ba84
Noslig <gilszon@gmail.com>
2021-02-05 01:38:47 -0300

    Strategy

---
+++ Strategy/ex02/src/ex02/Orcamento.java
@@ -0,0 +1,15 @@
+package ex02;
+
+public class Orcamento {
+
+	private final double valor;
+
+	public Orcamento(double valor) {
+		this.valor = valor;
+	}
+	
+	public double getValor() {
+		// TODO Auto-generated method stub
+		return valor;
+	}
+}
