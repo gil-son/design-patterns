865236dff3b77a5b573f83738807b1b87e0bc97e
gil-son <gilszon@gmail.com>
2021-03-11 18:28:05 -0300

    Factory

---
+++ Factory/src/EnemyShip.java
@@ -0,0 +1,18 @@
+public abstract class EnemyShip{
+	private String name;
+	private double Damage;
+	
+	
+	public String getName() {return name;}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getDamage() {
+		return Damage;
+	}
+	public void setAmtDamage(double amtDamage) {
+		this.Damage = Damage;
+	}
+	
+	
+}
