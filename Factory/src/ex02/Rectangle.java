7c761b1a4adf9b1cd331bcea320f7576898c5e05
gil-son <gilszon@gmail.com>
2021-04-30 02:40:00 -0300

    Ex02

---
+++ Factory/src/ex02/Rectangle.java
@@ -0,0 +1,10 @@
+package ex02;
+
+public class Rectangle implements Shape{
+
+	@Override
+	public void draw() {
+		System.out.println("Inside Rectangle::draw() method.");
+	}
+
+}
---
+++ Factory/src/ex02/Square.java
@@ -0,0 +1,10 @@
+package ex02;
+
+public class Square implements Shape{
+	
+	@Override
+	public void draw() {
+		System.out.println("Inside Square::draw() method.");
+		
+	}
+}
