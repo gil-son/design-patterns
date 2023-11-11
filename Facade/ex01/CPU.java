cc5f5e5ae6358316d18b3c0b5bf221d6cd83c159
gil-son <gilszon@gmail.com>
2023-11-11 16:53:29 -0300

    ex01 - Facade example

---
+++ Facade/ex01/CPU.java
@@ -0,0 +1,11 @@
+package ex01;
+
+public class CPU {
+    public void start() {
+        System.out.println("CPU is starting");
+    }
+
+    public void execute() {
+        System.out.println("CPU is executing");
+    }
+}
