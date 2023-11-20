87b00d9945da6fd7def123c0395417b8405955f6
gil-son <gilszon@gmail.com>
2023-11-19 21:54:57 -0300

    Structural

--- structural/Adapter/ex01/Target.java
+++
@@ -1,5 +0,0 @@
-package ex01;
-
-public interface Target {
-    void request();
-}
--- structural/Adapter/ex02/AnotherAdaptee.java
+++
@@ -1,8 +0,0 @@
-package ex02;
-
-// Another existing class with a different interface
-class AnotherAdaptee {
-    public void anotherSpecificRequest() {
-        System.out.println("AnotherAdaptee's specific request");
-    }
-}
